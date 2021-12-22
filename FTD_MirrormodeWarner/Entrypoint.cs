30ce7b898b013e656f9b28ad6e2087c5ab25441b
Punyo <49717092+Punyo@users.noreply.github.com>
2021-12-22 23:38:06 +0900

    プロジェクト ファイルを追加します。

---
+++ FTD_MirrormodeWarner/Entrypoint.cs
@@ -0,0 +1,36 @@
+﻿using BrilliantSkies.Modding;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTD_MirrormodeWarner
+{
+    public class Entrypoint : GamePlugin_PostLoad
+    {
+        public Entrypoint()
+        {
+        }
+
+        public string name => "FTD_Mirrormodewarner";
+
+        public Version version { get; }
+
+        public bool AfterAllPluginsLoaded()
+        {
+            return true;
+        }
+
+        public void OnLoad()
+        {
+            Harmony h = new Harmony("com.punyo.mirrormodewarner");
+            h.PatchAll();
+        }
+
+        public void OnSave()
+        {
+        }
+    }
+}
---
+++ FTD_MirrormodeWarner/Patcher_cBuild.cs
@@ -0,0 +1,28 @@
+﻿using BrilliantSkies.Ftd.Avatar.Build;
+using BrilliantSkies.Ui.Special.InfoStore;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTD_MirrormodeWarner
+{
+    [HarmonyPatch(typeof(cBuild), "RunUpdate")]
+    class Patcher_cBuild
+    {
+        private static void Postfix()
+        {
+            int id = cBuild.GetSingleton().GetCC().UniqueId;
+            if (!cBuild.GetSingleton().buildMarker.mirror.Info.On)
+            {
+                InfoStore.Add(id, "<color=red>[WARNING]Mirror mode disabled!!</color>");
+            }
+            else
+            {
+                InfoStore.RemoveMessage(id);
+            }
+        }
+    }
+}
